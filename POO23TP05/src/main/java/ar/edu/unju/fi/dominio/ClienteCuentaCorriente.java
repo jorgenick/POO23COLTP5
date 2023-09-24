4b2e9dc8b204e778e302587cb8fa33a05d74aa25
Usuario <Usuario@DESKTOP-P26PV15>
2023-09-24 17:53:16 -0300

    2comit

---
+++ POO23TP05/src/main/java/ar/edu/unju/fi/dominio/ClienteCuentaCorriente.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.dominio;
+
+public class ClienteCuentaCorriente extends Cliente {
+	/**
+	 * tienen la posibilidad de retirar materiales a cuenta con un tope de dinero
+	 */
+
+	public long montoMensualAFavor = 5000;
+
+	public ClienteCuentaCorriente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClienteCuentaCorriente(long dni, String nombre, String eMail, String localidad) {
+		super(dni, nombre, eMail, localidad);
+
+		this.montoMensualAFavor = montoMensualAFavor;
+	}
+
+	public long getMontoMensualAFavor() {
+		return montoMensualAFavor;
+	}
+
+	public void setMontoMensualAFavor(long montoMensualAFavor) {
+		this.montoMensualAFavor = montoMensualAFavor;
+	}
+
+}
---
+++ POO23TP05/src/main/java/ar/edu/unju/fi/dominio/ClienteGrandesCompras.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.dominio;
+
+public class ClienteGrandesCompras extends Cliente {
+	/**
+	 * tienen un determinado descuento
+	 */
+	double porcentajeDescuento = 0.5;
+
+	public ClienteGrandesCompras() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClienteGrandesCompras(long dni, String nombre, String eMail, String localidad) {
+		super(dni, nombre, eMail, localidad);
+
+		this.porcentajeDescuento = porcentajeDescuento;
+	}
+
+	public double getPorcentajeDescuento() {
+		return porcentajeDescuento;
+	}
+
+	public void setPorcentajeDescuento(double porcentajeDescuento) {
+		this.porcentajeDescuento = porcentajeDescuento;
+	}
+
+}
